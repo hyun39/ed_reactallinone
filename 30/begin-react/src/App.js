fad2a902d5565e9841071440e2dedcb0442b33cc
manure39 <manure39@gmail.com>
2022-03-22 17:13:25 +0900

    04.jsx #3

--- 30/begin-react/src/App.js
+++ 30/begin-react/src/App.js
@@ -6,10 +6,21 @@ import Hello  from './Hello'
 
 
 function App() {
+
+  const name ='react';
+  const style = {
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
-    <div >
+    <>
+      {/* zzzzz */}
       <Hello/>
-    </div>
+      <div  style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
--- readme.txt
+++ readme.txt
@@ -5,6 +5,15 @@ cd begin-reaact
 yarn start  or npm start
 
 
+- 04 JSX - 
+
+https://babeljs.io/
+
+JSX의 규칙
+1. 반드시 닫아야 한다.
+2. 2개 이상의 태그는 하나의 태그에 의해 담겨져 있어야 한다.  <>  </> 이걸로 해도 된다.
+
+
 
 
 
