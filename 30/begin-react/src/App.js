906055204aa81d1a1aa3d431d55a27ba38b80e65
manure39 <manure39@gmail.com>
2022-03-22 18:04:40 +0900

    07.usestate #6

--- 30/begin-react/src/App.js
+++ 30/begin-react/src/App.js
@@ -2,19 +2,13 @@
 
 import React from 'react'
 
-import Hello  from './Hello'
-import Wrapper from './Wrapper';
-
-
-
+import Counter from './Conter';
 
 function App() {
 
   return (
-    <Wrapper>
-      <Hello name="react" color ="red" isSpecial/>
-      <Hello  color ="pink"/>
-    </Wrapper>
+
+    <Counter/>
   );
 }
 
