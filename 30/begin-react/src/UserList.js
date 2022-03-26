7f787211bc7509971351ecd519f60b3841f1e93d
manure39 <manure39@gmail.com>
2022-03-26 22:17:02 +0900

    30.16 useEffect를 사용하여 마운트언마운트업데이트시 할 작업 설정하기 #17

--- 30/begin-react/src/UserList.js
+++ 30/begin-react/src/UserList.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 
 
 function User({user, onRemove, onToggle}){
 
     const {username,email, id, active} = user;
+
+    // useEffect(()=>{
+
+    //     console.log("컴포넌트가 화면에 나타남");
+    //
+    //     return () => {
+    //         console.log("컴포넌트가 사라짐")
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        // 마운트 ( 초기, 수정 후)
+        console.log("값 설정");
+        console.log(user)
+
+        return () => {
+            // unmound (수정 전)
+            console.log("값 변경 전")
+            console.log(user)
+        }
+
+        // 아래 파티미터가 들어가 있는 것을 기억해야 한다.
+    }, [user]);
+
+
     return (
         <div>
         <b 
